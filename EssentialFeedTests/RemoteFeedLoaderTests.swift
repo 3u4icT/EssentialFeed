5afc66aa5be347257f580ef13f1740d374f4f136
Sarvjeet Singh <sarvjeet.singh@byjus.com>
2022-04-07 21:52:07 +0530

    Simplify spy

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -15,7 +15,7 @@ class RemoteFeedLoaderTests : XCTestCase{
         
         let (_, client) = makeSUT()
     
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     
@@ -24,7 +24,7 @@ class RemoteFeedLoaderTests : XCTestCase{
         let url = URL.init(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url : url)
         sut.load()
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
@@ -47,11 +47,9 @@ class RemoteFeedLoaderTests : XCTestCase{
     
     private class HTTPClientSpy: HTTPClient{
         
-        var requestedURL : URL?
         var requestedURLs = [URL]()
         
          func get(from url: URL) {
-            requestedURL = url
             requestedURLs.append(url)
         }
         
